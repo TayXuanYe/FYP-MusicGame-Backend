5151d08c0c364b5b0275f15c6bf79e0ab506ba89
TayXuanYe <pikaqiu04082004@gmail.com>
2025-08-30 23:40:13 +0800

    Nuget add

--- Program.cs
+++ Program.cs
@@ -11,3 +11,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 // configuring database context
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+var app = builder.Build();
