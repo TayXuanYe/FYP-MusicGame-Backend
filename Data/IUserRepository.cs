95261c25504ae257d5d1d5560ce636e7820768db
TayXuanYe <pikaqiu04082004@gmail.com>
2025-08-30 20:57:10 +0800

    Done user repository [data layer]

--- Data/IUserRepository.cs
+++ Data/IUserRepository.cs
@@ -5,8 +5,8 @@ using FYP_MusicGame_Backend.Models;
 public interface IUserRepository
 {
     Task<IEnumerable<User>> GetAllUsersAsync();
-    Task<User> GetUserByIdAsync();
-    Task<User> GetUserByNameAsync();
+    Task<User?> GetUserByIdAsync(int id);
+    Task<User?> GetUserByUsernameAsync(string name);
     Task AddUserAsync(User user);
     Task UpdateUserAsync(User user);
     Task DeleteUserAsync(int id);
