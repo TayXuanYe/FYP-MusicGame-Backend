3b6dd3c06f540c96d94be0f6ab1c01c7be6942a6
TayXuanYe <pikaqiu04082004@gmail.com>
2025-10-05 14:47:03 +0800

    Feat: Add BugReports table and remove Score column from GameHistories

---
+++ Migrations/20251005062148_AddBugReportTable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FYP_MusicGame_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBugReportTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "GameHistories");
+
+            migrationBuilder.CreateTable(
+                name: "BugReports",
+                columns: table => new
+                {
+                    ReportId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ReporterId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    StepsToReproduce = table.Column<string>(type: "TEXT", nullable: false),
+                    ReportTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BugReports", x => x.ReportId);
+                    table.ForeignKey(
+                        name: "FK_BugReports_Users_ReporterId",
+                        column: x => x.ReporterId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BugReports_ReporterId",
+                table: "BugReports",
+                column: "ReporterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BugReports");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "GameHistories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
