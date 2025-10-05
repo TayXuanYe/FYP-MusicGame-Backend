d042670444985aa46e3d0096ff8c4669c8d90c67
TayXuanYe <pikaqiu04082004@gmail.com>
2025-10-05 14:49:56 +0800

    Feat: Modify BugReport table to make StepsToReproduce nullable

---
+++ Migrations/20251005064929_ModifyBugReportTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FYP_MusicGame_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyBugReportTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StepsToReproduce",
+                table: "BugReports",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StepsToReproduce",
+                table: "BugReports",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
