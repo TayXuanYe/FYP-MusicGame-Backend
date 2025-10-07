6f7e4b78d45fdff718c84dbdb62b06c242c09d5d
TayXuanYe <pikaqiu04082004@gmail.com>
2025-10-07 13:00:30 +0800

    Add TrackNo field to GameHistories table and update model snapshot

--- Migrations/20251007045920_AddGameHistoryTableNewFieldTrackNo.Designer.cs
+++ Migrations/20251007045920_AddGameHistoryTableNewFieldTrackNo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FYP_MusicGame_Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FYP_MusicGame_Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251007045920_AddGameHistoryTableNewFieldTrackNo")]
+    partial class AddGameHistoryTableNewFieldTrackNo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");
